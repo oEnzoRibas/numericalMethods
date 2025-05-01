823529b00fb88de2648229210921bd8e1d77040d
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-05-01 20:07:17 -0300

    fixing the casting

--- Projects/Machine_Epsilon/python/machine_epsilon.py
+++ Projects/Machine_Epsilon/python/machine_epsilon.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def machine_precision_single(VAL):
+    """
+    This function calculates the machine epsilon for single-precision floating-point numbers
+    for a given value. It iteratively halves the value of `aSingle` until the sum of `VAL` 
+    and `aSingle` is indistinguishable from `VAL`. The machine epsilon is then calculated 
+    as twice the final value of `aSingle`.
+
+    :param VAL: The value for which the machine epsilon is to be calculated.
+    :type VAL: np.float32
+
+    """
     aSingle = np.float32(1)
     sSingle = np.float32(VAL + aSingle)
 
@@ -19,7 +29,7 @@ def machine_precision_double(VAL):
     as twice the final value of `aDouble`.
 
     :param VAL: The value for which the machine epsilon is to be calculated.
-    :type VAL: float
+    :type VAL: np.float64
 
     """
 
@@ -32,29 +42,6 @@ def machine_precision_double(VAL):
 
     prec =  np.float64(2)  * aDouble
     return prec
-    
-    #128 may not be supported in all systems
-def machine_precision_longdouble(VAL):
-    """
-    This function calculates the machine epsilon for long double-precision floating-point numbers
-    for a given value. It iteratively halves the value of `aLongDouble` until the sum of `VAL` 
-    and `aLongDouble` is indistinguishable from `VAL`. The machine epsilon is then calculated 
-    as twice the final value of `aLongDouble`.
-
-    :param VAL: The value for which the machine epsilon is to be calculated.
-    :type VAL: float
-
-    """
-
-    aDouble = np.float128(1)
-    sDouble = np.float128(VAL + aDouble)
-
-    while( sDouble > VAL):
-        aDouble = aDouble/np.float128(2)
-        sDouble = np.float128(VAL + aDouble)
-
-    prec = np.float128(2) * aDouble
-    return prec
 
 def main():
     """
@@ -65,12 +52,8 @@ def main():
 
     nums = [1,10,17,100,184,1000,1575,10000,17893]
     for val in  nums:
-        print(f"Machine Epsilon Single for val {val}: {machine_precision_single(np.float32(val))}")
-    for val in nums:
-        print(f"Machine Epsilon Double for val {val}: {machine_precision_double(np.float64(val))}")
-    
-    #128 may not be supported in all systems
-    for val in nums:
-        print(f"Machine Epsilon Double for val {val}: {machine_precision_longdouble(np.float128(val))}")
+        print(f"M.E. Single for val {val}: {machine_precision_single(np.float32(val))}")
+        
+        print(f"ME. Double for val {val}: {machine_precision_double(np.float64(val))}")
 
 main()
